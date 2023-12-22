79e231c849cdba07926508b1ce87626e7f34e02c
Jijun Leng <jijunleng@gmail.com>
2023-12-21 23:33:39 -0800

    feat: redis with password protection and RBAC

--- light/cluster/redis.py
+++ light/cluster/redis.py
@@ -2,6 +2,7 @@ import pulumi
 from pulumi_kubernetes.helm.v3 import Chart, ChartOpts, FetchOpts
 from light.utils import call_once
 import pulumi_kubernetes as k8s
+import secrets
 
 
 @call_once
@@ -13,6 +14,10 @@ def create_redis(k8s_provider: k8s.Provider) -> None:
         "redis",
         metadata={"name": "redis"},
     )
+
+    # Generate a 32-character random password
+    password = secrets.token_hex(16)
+
     Chart(
         "redis",
         ChartOpts(
@@ -22,10 +27,33 @@ def create_redis(k8s_provider: k8s.Provider) -> None:
             fetch_opts=FetchOpts(repo="https://charts.bitnami.com/bitnami"),
             values={
                 "architecture": "standalone",  # Use "replication" for high availability
-                "auth": {"enabled": False},
+                "auth": {"enabled": True, "password": password},
                 # "master": {"persistence": {"enabled": True, "size": "2Gi"}},
                 "metrics": {"enabled": True},  # For enabling metrics
             },
         ),
+        opts=pulumi.ResourceOptions(provider=k8s_provider),
+    )
+
+    # Create a Kubernetes Secret to store the password
+    secret = k8s.core.v1.Secret(
+        "redis-password",
+        metadata={"name": "redis-password", "namespace": "redis"},
+        string_data={"password": password},
+        opts=pulumi.ResourceOptions(provider=k8s_provider),
+    )
+
+    # Create a Kubernetes Role that allows reading the secret
+    k8s.rbac.v1.Role(
+        "redis-secret-reader",
+        metadata={"namespace": "redis"},
+        rules=[
+            {
+                "apiGroups": [""],
+                "resources": ["secrets"],
+                "verbs": ["get", "watch", "list"],
+                "resourceNames": [secret.metadata["name"]],
+            }
+        ],
         opts=pulumi.ResourceOptions(provider=k8s_provider),
     )
