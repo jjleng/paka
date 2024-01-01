fc455bcfe77343fcf2ab7e8187a4a50cb28e8e1f
Jijun Leng <jijunleng@gmail.com>
2024-01-01 14:06:18 -0800

    refactor: move cli to a package

---
+++ light/cli/__init__.py
@@ -0,0 +1,68 @@
+import typer
+from light.cluster.manager.aws import AWSClusterManager
+from light.config import CloudConfig, ClusterConfig, Config, CloudModelGroup
+from light.job.celery_worker import create_celery_workers
+
+cli = typer.Typer()
+
+cluster_manager = AWSClusterManager(
+    config=Config(
+        aws=CloudConfig(
+            cluster=ClusterConfig(name="open-copilot", defaultRegion="us-west-2"),
+            modelGroups=[
+                CloudModelGroup(
+                    name="llama-2-7b.Q4_0.gguf",
+                    maxInstances=2,
+                    minInstances=0,
+                    nodeType="m5.xlarge",
+                ),
+            ],
+        )
+    )
+)
+
+cluster_app = typer.Typer()
+
+
+@cluster_app.command()
+def up() -> None:
+    cluster_manager.create()
+
+
+@cluster_app.command()
+def down() -> None:
+    cluster_manager.destroy()
+
+
+@cluster_app.command()
+def refresh() -> None:
+    cluster_manager.refresh()
+
+
+@cluster_app.command()
+def preview() -> None:
+    cluster_manager.preview()
+
+
+cli.add_typer(cluster_app, name="cluster")
+
+service_app = typer.Typer()
+
+
+@service_app.command("up")
+def service_up() -> None:
+    cluster_manager.service_up()
+
+
+cli.add_typer(service_app, name="service")
+
+
+@cli.command()
+def pkg_info() -> None:
+    create_celery_workers(
+        cluster_manager.config, "celery -A tasks worker -l INFO", "simple-celery"
+    )
+
+
+if __name__ == "__main__":
+    cli()
