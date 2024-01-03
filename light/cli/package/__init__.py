42301f7bbea8d5e50663e2c1a5edb09cbb0e00ab
Jijun Leng <jijunleng@gmail.com>
2024-01-03 15:27:54 -0800

    refactor: move package/typer.tpy to package/__init__.py

---
+++ light/cli/package/__init__.py
@@ -0,0 +1,75 @@
+import typer
+import tempfile
+import os
+import re
+import time
+from light.cli.package.archive import archive_directory
+from light.cli.package.ignore import blacklist
+from light.cli.fission.package import upsert_package, get_package
+from light.logger import setup_logger, logger
+
+
+package_app = typer.Typer()
+
+
+@package_app.command("create")
+def package_create(
+    name: str = typer.Argument(
+        ...,
+        help="The package name",
+    ),
+    source_directory: str = typer.Option(
+        ...,
+        "--source",
+        "-s",
+        help="The source directory to create the package from",
+    ),
+    env: str = typer.Option(
+        ...,
+        "--env",
+        "-e",
+        help="The environment to use for the package. Supported environments are 'python3.12', 'node18', etc.",
+    ),
+    verbose: bool = typer.Option(
+        False, "--verbose", "-v", is_flag=True, help="Enable verbose output"
+    ),
+) -> None:
+    setup_logger(verbose)
+
+    if (
+        not re.match(
+            r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$", name
+        )
+        or len(name) > 63
+    ):
+        logger.info(
+            "Invalid name. It must contain no more than 63 characters, contain only lowercase alphanumeric characters, '-' or '.', start with an alphanumeric character, and end with an alphanumeric character."
+        )
+        raise typer.Exit(1)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        archive_path = os.path.join(temp_dir, name)
+        archive_directory(source_directory, archive_path, blacklist)
+        typer.echo(f"Archive '{archive_path}.zip' created successfully.")
+        upsert_package(
+            "open-copilot", name, "default", env, f"{archive_path}.zip", "./build.sh"
+        )
+        package = get_package("open-copilot", name, "default")
+        status = package["status"]
+
+        if status["buildstatus"] in ["pending", "running"]:
+            logger.info("Package building")
+
+        while status["buildstatus"] in ["pending", "running"]:
+            package = get_package("open-copilot", name, "default")
+            status = package["status"]
+            print(".", end="", flush=True)
+            time.sleep(1)
+
+        logger.info("")
+        logger.debug(status.get("buildlog", ""))
+
+        if status["buildstatus"] != "succeeded":
+            logger.info("Package build failed.")
+        else:
+            logger.info("Package build succeeded.")
