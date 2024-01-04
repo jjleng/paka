689fd43dad6efc3640dc8b1852565f7c7c1225f7
Jijun Leng <jijunleng@gmail.com>
2024-01-03 16:12:31 -0800

    fix: be more restrictive about k8s name validation

--- light/cli/utils.py
+++ light/cli/utils.py
@@ -10,13 +10,13 @@ def validate_name(func: Any) -> Any:
     def wrapper(name: str, *args: Any, **kwargs: Any) -> Any:
         if (
             not re.match(
-                r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$",
+                r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?$",
                 name,
             )
             or len(name) > 63
         ):
             logger.info(
-                "Invalid name. It must contain no more than 63 characters, contain only lowercase alphanumeric characters, '-' or '.', start with an alphanumeric character, and end with an alphanumeric character."
+                "Invalid name. It must contain no more than 63 characters, contain only lowercase alphanumeric characters or '-', start with an alphanumeric character, and end with an alphanumeric character."
             )
             raise typer.Exit(1)
         return func(name, *args, **kwargs)
