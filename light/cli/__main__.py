1211000fdbcd4b60f7524c7ea3330bc58005f2c7
Jijun Leng <jijunleng@gmail.com>
2024-01-04 23:28:40 -0800

    feat: command to deploy a function (WIP)

--- light/cli/__main__.py
+++ light/cli/__main__.py
@@ -6,6 +6,7 @@ from light.cli.env import env_app
 from light.cli.archive import archive_app
 from light.cli.function import function_app
 from light.cli.spec import spec_app
+from light.cli.deploy import deploy_app
 from light.logger import setup_logger
 
 
@@ -82,6 +83,8 @@ cli.add_typer(archive_app, name="archive")
 cli.add_typer(function_app, name="fn")
 
 cli.add_typer(spec_app, name="spec")
+
+cli.add_typer(deploy_app, name="deploy")
 
 
 def main() -> None:
---
+++ light/cli/deploy.py
@@ -0,0 +1,63 @@
+import typer
+from ruamel.yaml import YAML
+from light.logger import logger
+from light.cli.spec.schema import APP_KIND_FUNCTION, FunctionSpec
+from kubernetes import config
+from light.cli.fission.env import upsert_env
+from light.cli.env import pick_runtime
+
+APP_NS = "default"
+JOB_NS = "jobs"
+
+deploy_app = typer.Typer()
+
+
+def deploy_function(spec: FunctionSpec, build_command: str) -> None:
+    logger.info(f"Deploying function spec '{spec.name}'")
+
+    # Create the runtime env. If the env does not exist, we need to create it.
+    # If the env exists, we need to update it.
+    image, builder_image = pick_runtime(spec.runtime)
+    language, _ = spec.runtime.split(":")
+
+    if language == "python" and not build_command:
+        build_command = r"sh -c 'pip3 install -r ${SRC_PKG}/requirements.txt -t ${SRC_PKG} && cp -r ${SRC_PKG} ${DEPLOY_PKG}'"
+
+    upsert_env(
+        "open-copilot",
+        spec.name,
+        APP_NS,
+        image=image,
+        builder_image=builder_image,
+        builder_command=build_command,
+    )
+
+    # Create the package.
+
+
+@deploy_app.callback(invoke_without_command=True)
+def deploy(
+    spec: str = typer.Argument(
+        ...,
+        help="Path of the spec file.",
+    ),
+    kubeconfig: str = typer.Option(
+        ...,
+        "--kubeconfig",
+        "-k",
+        help="Path of the kubeconfig file.",
+    ),
+    build_command: str = typer.Option(
+        "",
+        "--build-command",
+        "-b",
+        help="The command to build the function.",
+    ),
+) -> None:
+    config.load_kube_config(kubeconfig)
+    with open(spec, "r") as f:
+        file_data = f.read()
+        yaml = YAML()
+        yaml_data = yaml.load(file_data)
+        if yaml_data["kind"] == APP_KIND_FUNCTION:
+            deploy_function(FunctionSpec(**yaml_data), build_command)
