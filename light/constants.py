cd06896855c3f190db3dabbb73bfbab7c4d62f80
Jijun Leng <jijunleng@gmail.com>
2024-01-09 14:23:51 -0800

    fix: better service account name for model groups

--- light/constants.py
+++ light/constants.py
@@ -1,6 +1,6 @@
 PROJECT_NAME = "light"
 # Model group service account
-MODEL_GROUP_SA = "light-sa"
+MODEL_GROUP_SA = "model-group-sa"
 # Celery worker service account
 CELERY_WORKER_SA = "celery-worker-sa"
 # Namespace for redis and workers
