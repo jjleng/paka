74f9ccf439191656ff20bc6f0d8641aacda5b2bf
Jijun Leng <jijunleng@gmail.com>
2024-01-09 15:48:21 -0800

    feat: drain celery worker deployment if it already exists

--- light/job/celery_worker.py
+++ light/job/celery_worker.py
@@ -1,10 +1,30 @@
 import shlex
+import time
 
 from kubernetes import client
 
 from light.constants import CELERY_WORKER_SA, JOB_NS
 from light.job.autoscaler import create_autoscaler
-from light.k8s import apply_resource, create_namespace, create_service_account
+from light.k8s import (
+    apply_resource,
+    create_namespace,
+    create_service_account,
+    try_load_kubeconfig,
+)
+from light.logger import logger
+
+try_load_kubeconfig()
+
+
+def wait_for_pods_to_drain(namespace: str, deployment_name: str) -> None:
+    while True:
+        pods = client.CoreV1Api().list_namespaced_pod(
+            namespace, label_selector=f"app={deployment_name}"
+        )
+        if not pods.items:
+            break
+        logger.info(f"Waiting for {len(pods.items)} pod(s) to drain...")
+        time.sleep(10)
 
 
 def create_deployment(
@@ -37,10 +57,10 @@ def create_deployment(
         kind="Deployment",
         metadata=client.V1ObjectMeta(name=deployment_name, namespace=namespace),
         spec=client.V1DeploymentSpec(
-            replicas=1,
-            selector=client.V1LabelSelector(match_labels={"name": deployment_name}),
+            replicas=0,
+            selector=client.V1LabelSelector(match_labels={"app": deployment_name}),
             template=client.V1PodTemplateSpec(
-                metadata=client.V1ObjectMeta(labels={"name": deployment_name}),
+                metadata=client.V1ObjectMeta(labels={"app": deployment_name}),
                 spec=client.V1PodSpec(
                     service_account_name=service_account_name,
                     containers=containers,
@@ -66,6 +86,12 @@ def create_celery_workers(
 
     deployment_name = task_name
 
+    if drain_existing_task:
+        # Check if the deployment already exists
+        # Wait for pods to drain
+        wait_for_pods_to_drain(namespace, deployment_name)
+
+    # Otherwise, upsert the deployment. This will update the deployment if it already exists.
     create_deployment(
         runtime_command,
         namespace,
@@ -80,6 +106,6 @@ def create_celery_workers(
         queue_name="0",
         trigger_queue_length=5,
         deployment_name=deployment_name,
-        min_replicas=1,
+        min_replicas=0,  # Hard coded, scale to 0
         max_replicas=5,
     )
