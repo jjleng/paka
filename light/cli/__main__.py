4259ab1b00cb7cdc5ce5be02c7cf01e5f4d8cb60
Jijun Leng <jijunleng@gmail.com>
2024-01-21 20:55:20 -0800

    refactor: a cluster app for cluster command

--- light/cli/__main__.py
+++ light/cli/__main__.py
@@ -1,14 +1,13 @@
 import typer
 
 from light.cli.build import build_app
+from light.cli.cluster import cluster_app
 from light.cli.function import function_app
 from light.cli.job import job_app
 from light.cli.kubeconfig import kube_app
 from light.cli.model_group import model_group_app
 from light.cli.run import run_app
-from light.cli.utils import load_cluster_manager
-from light.k8s import update_kubeconfig as merge_update_kubeconfig
-from light.logger import logger, setup_logger
+from light.logger import setup_logger
 
 
 def verbose_option(
@@ -21,72 +20,6 @@ def verbose_option(
 
 cli = typer.Typer()
 cli.callback()(verbose_option)
-
-
-cluster_app = typer.Typer()
-
-
-@cluster_app.command()
-def up(
-    cluster_config: str = typer.Option(
-        "",
-        "--file",
-        "-f",
-        help="Path to the cluster config file.",
-    ),
-    update_kubeconfig: bool = typer.Option(
-        False,
-        "--update-kubeconfig",
-        "-u",
-        help="Update kubeconfig with the new cluster.",
-    ),
-) -> None:
-    cluster_manager = load_cluster_manager(cluster_config)
-    cluster_manager.create()
-    if update_kubeconfig:
-        logger.info("Updating kubeconfig...")
-        merge_update_kubeconfig()
-        logger.info("Successfully updated kubeconfig.")
-
-
-@cluster_app.command()
-def down(
-    cluster_config: str = typer.Option(
-        "",
-        "--file",
-        "-f",
-        help="Path to the cluster config file.",
-    ),
-) -> None:
-    cluster_manager = load_cluster_manager(cluster_config)
-    cluster_manager.destroy()
-
-
-@cluster_app.command()
-def refresh(
-    cluster_config: str = typer.Option(
-        "",
-        "--file",
-        "-f",
-        help="Path to the cluster config file.",
-    ),
-) -> None:
-    cluster_manager = load_cluster_manager(cluster_config)
-    cluster_manager.refresh()
-
-
-@cluster_app.command()
-def preview(
-    cluster_config: str = typer.Option(
-        "",
-        "--file",
-        "-f",
-        help="Path to the cluster config file.",
-    ),
-) -> None:
-    cluster_manager = load_cluster_manager(cluster_config)
-    cluster_manager.preview()
-
 
 cli.add_typer(cluster_app, name="cluster")
 
---
+++ light/cli/cluster.py
@@ -0,0 +1,69 @@
+import typer
+
+from light.cli.utils import load_cluster_manager
+from light.k8s import update_kubeconfig as merge_update_kubeconfig
+from light.logger import logger
+
+cluster_app = typer.Typer()
+
+
+@cluster_app.command()
+def up(
+    cluster_config: str = typer.Option(
+        "",
+        "--file",
+        "-f",
+        help="Path to the cluster config file.",
+    ),
+    update_kubeconfig: bool = typer.Option(
+        False,
+        "--update-kubeconfig",
+        "-u",
+        help="Update kubeconfig with the new cluster.",
+    ),
+) -> None:
+    cluster_manager = load_cluster_manager(cluster_config)
+    cluster_manager.create()
+    if update_kubeconfig:
+        logger.info("Updating kubeconfig...")
+        merge_update_kubeconfig()
+        logger.info("Successfully updated kubeconfig.")
+
+
+@cluster_app.command()
+def down(
+    cluster_config: str = typer.Option(
+        "",
+        "--file",
+        "-f",
+        help="Path to the cluster config file.",
+    ),
+) -> None:
+    cluster_manager = load_cluster_manager(cluster_config)
+    cluster_manager.destroy()
+
+
+@cluster_app.command()
+def refresh(
+    cluster_config: str = typer.Option(
+        "",
+        "--file",
+        "-f",
+        help="Path to the cluster config file.",
+    ),
+) -> None:
+    cluster_manager = load_cluster_manager(cluster_config)
+    cluster_manager.refresh()
+
+
+@cluster_app.command()
+def preview(
+    cluster_config: str = typer.Option(
+        "",
+        "--file",
+        "-f",
+        help="Path to the cluster config file.",
+    ),
+) -> None:
+    cluster_manager = load_cluster_manager(cluster_config)
+    cluster_manager.preview()
