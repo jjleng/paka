bef95d88a3acb6ebf116e1302e5e7855156331d4
Jijun Leng <jijunleng@gmail.com>
2024-01-22 16:29:25 -0800

    fix: fix the failing tests for kubeconfig file saving

--- tests/test_k8s.py
+++ tests/test_k8s.py
@@ -7,10 +7,11 @@ from light.utils import get_project_data_dir, save_kubeconfig
 
 def test_save_kubeconfig() -> None:
     m = mock_open()
+    # Replace the built-in open function with the mock object
     with patch("builtins.open", m):
         kubeconfig_json = json.dumps({"apiVersion": "v1"})
         save_kubeconfig("test", kubeconfig_json)
-    f = os.path.join(get_project_data_dir(), "test", "kubeconfig.yaml")
+    f = os.path.join(get_project_data_dir(), "clusters", "test", "kubeconfig.yaml")
     m.assert_called_once_with(f, "w")
     handle = m()
     handle.write.assert_called_once()
