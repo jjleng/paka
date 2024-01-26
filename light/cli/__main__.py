5fb30559e697ff8d70166e5cca6467ae15a6d8ad
Jijun Leng <jijunleng@gmail.com>
2024-01-25 23:12:50 -0800

    feat: help messages for CLI sub-commands

--- light/cli/__main__.py
+++ light/cli/__main__.py
@@ -24,19 +24,19 @@ def verbose_option(
 cli = typer.Typer()
 cli.callback()(verbose_option)
 
-cli.add_typer(cluster_app, name="cluster")
+cli.add_typer(cluster_app, name="cluster", help="Manage clusters.")
 
-cli.add_typer(job_app, name="job")
+cli.add_typer(job_app, name="job", help="Manage batch jobs.")
 
-cli.add_typer(build_app, name="build")
+cli.add_typer(build_app, name="build", help="Build Docker images.")
 
-cli.add_typer(kube_app, name="kubeconfig")
+cli.add_typer(kube_app, name="kubeconfig", help="Export kubeconfig.")
 
-cli.add_typer(run_app, name="run")
+cli.add_typer(run_app, name="run", help="Run one-off script.")
 
-cli.add_typer(function_app, name="function")
+cli.add_typer(function_app, name="function", help="Manage serverless functions.")
 
-cli.add_typer(model_group_app, name="model-group")
+cli.add_typer(model_group_app, name="model-group", help="Manage model groups.")
 
 
 def main() -> None:
