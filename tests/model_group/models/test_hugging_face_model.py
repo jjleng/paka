64b5b0b3bcfca0357dda73d87afa7f8de7190b5f
SoftewareArtist <heavy.sheng.yu@gmail.com>
2024-04-12 07:36:24 +0800

    Fix feedbacks

--- tests/model_group/models/test_hugging_face_model.py
+++ tests/model_group/models/test_hugging_face_model.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import ANY, Mock, patch
+from unittest.mock import ANY, MagicMock, Mock, patch
 
 from paka.kube_resources.model_group.models.hugging_face_model import (
     HuggingFaceModel,  # replace with the actual module name
@@ -8,19 +8,18 @@ from paka.kube_resources.model_group.models.hugging_face_model import (
 
 class TestHuggingFaceModel(unittest.TestCase):
     def setUp(self) -> None:
-        self.model = HuggingFaceModel(
-            "TheBloke/Llama-2-7B-Chat-GGUF",
-            files=[
-                "llama-2-7b-chat.Q4_0.gguf",
-                "llama-2-7b-chat.Q2_K.gguf",
-            ],
-        )
-
-    def test_validate_files(self) -> None:
-        # Assert
-        self.assertEqual(len(self.model.files), 2)
-        for file in self.model.files:
-            self.assertTrue(file.startswith(self.model.repo_id))
+        with patch.object(HuggingFaceModel, "validate_files") as mock_validate_files:
+            mock_validate_files.return_value = [
+                "TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_0.gguf",
+                "TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q2_K.gguf",
+            ]
+            self.model = HuggingFaceModel(
+                "TheBloke/Llama-2-7B-Chat-GGUF",
+                files=[
+                    "llama-2-7b-chat.Q4_0.gguf",
+                    "llama-2-7b-chat.Q2_K.gguf",
+                ],
+            )
 
     @patch.object(HuggingFaceModel, "get_file_info")
     @patch.object(HuggingFaceModel, "upload_fs_to_s3")
@@ -35,9 +34,9 @@ class TestHuggingFaceModel(unittest.TestCase):
     ) -> None:
         mock_s3_file_exists.return_value = False
         mock_upload_fs_to_s3.return_value = (
-            "test_upload_id",
-            "9958ee9b670594147b750bbc7d0540b928fa12dcc5dd4c58cc56ed2eb85e371b",
+            "9958ee9b670594147b750bbc7d0540b928fa12dcc5dd4c58cc56ed2eb85e371b"
         )
+
         hf_file_path = "TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_0.gguf"
         full_model_file_path = self.model.get_s3_file_path(hf_file_path)
         mock_get_file_info.return_value = {
@@ -50,14 +49,22 @@ class TestHuggingFaceModel(unittest.TestCase):
         }
 
         # Act
+        self.model.s3 = MagicMock()
+        self.model.s3.create_multipart_upload = MagicMock()
+        self.model.s3.create_multipart_upload.return_value = {"UploadId": "test"}
         self.model.upload_file_to_s3(hf_file_path)
 
         # Assert
         mock_get_file_info.assert_called_once_with(hf_file_path)
-        mock_upload_fs_to_s3.assert_called_once_with(ANY, 1024, full_model_file_path)
+        mock_upload_fs_to_s3.assert_called_once_with(
+            ANY, 1024, full_model_file_path, "test"
+        )
 
     @patch.object(HuggingFaceModel, "upload_file_to_s3")
-    def test_upload_files(self, mock_upload_file_to_s3: Mock) -> None:
+    @patch.object(HuggingFaceModel, "save_manifest_yml")
+    def test_upload_files(
+        self, mock_save_manifest_yml: Mock, mock_upload_file_to_s3: Mock
+    ) -> None:
         # Act
         self.model.upload_files()
 
