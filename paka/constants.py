d0fd49f0cf4a8d6776a4b7fb867b57eef2110af6
Jijun Leng <jijunleng@gmail.com>
2024-04-25 17:35:59 -0700

    refactor: get rid of hard coded model mount path in containers

--- paka/constants.py
+++ paka/constants.py
@@ -1,4 +1,14 @@
+# The name of the project
 PROJECT_NAME = "paka"
+
+# The service account that has access to all resources
 ACCESS_ALL_SA = "access-all-sa"
+
+# The environment variable for the directory where the paka data is saved
 HOME_ENV_VAR = "PAKA_HOME"
+
+# The environment variable for the buildpack builder
 BP_BUILDER_ENV_VAR = "BP_BUILDER"
+
+# The path where the model files are mounted in the container
+MODEL_MOUNT_PATH = "/data"
