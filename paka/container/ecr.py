ada6ec935727397945192b77411303f410fda107
Jijun Leng <jijunleng@gmail.com>
2024-04-28 17:00:36 -0700

    refactor: make docker login more secure

--- paka/container/ecr.py
+++ paka/container/ecr.py
@@ -7,23 +7,31 @@ from paka.logger import logger
 from paka.utils import random_str
 
 
-def authenticate_docker_to_ecr(
-    aws_region: str,
-) -> str:
-    ecr_client = boto3.client("ecr", region_name=aws_region)
-    token = ecr_client.get_authorization_token()
-    username, password = (
-        base64.b64decode(token["authorizationData"][0]["authorizationToken"])
-        .decode("utf-8")
-        .split(":")
-    )
-    ecr_url = token["authorizationData"][0]["proxyEndpoint"]
+def authenticate_docker_to_ecr(aws_region: str) -> str:
+    try:
+        ecr_client = boto3.client("ecr", region_name=aws_region)
+        token = ecr_client.get_authorization_token()
+        username, password = (
+            base64.b64decode(token["authorizationData"][0]["authorizationToken"])
+            .decode("utf-8")
+            .split(":")
+        )
+        ecr_url = token["authorizationData"][0]["proxyEndpoint"]
 
-    # Authenticate Docker to the ECR
-    subprocess.run(
-        ["docker", "login", "-u", username, "-p", password, ecr_url], check=True
-    )
-    return ecr_url
+        p = subprocess.Popen(
+            ["docker", "login", "-u", username, "--password-stdin", ecr_url],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = p.communicate(input=password.encode())
+        if p.returncode != 0:
+            raise Exception(f"Docker login failed: {stderr.decode()}")
+
+        return ecr_url
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise
 
 
 def push_to_ecr(
