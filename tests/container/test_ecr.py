527a24c4ede27d5028bf087cd67e0343983cc228
Jijun Leng <jijunleng@gmail.com>
2024-04-28 17:00:36 -0700

    refactor: make docker login more secure

--- tests/container/test_ecr.py
+++ tests/container/test_ecr.py
@@ -1,4 +1,4 @@
-from unittest.mock import ANY, patch
+from unittest.mock import MagicMock, patch
 
 from moto import mock_aws
 
@@ -7,8 +7,10 @@ from paka.container.ecr import authenticate_docker_to_ecr
 
 @mock_aws
 def test_authenticate_docker_to_ecr() -> None:
-    with patch("subprocess.run") as mock_subprocess:
+    with patch("subprocess.Popen") as mock_popen:
+        mock_result = MagicMock()
+        mock_result.communicate.return_value = (b"", b"")
+        mock_result.returncode = 0
+        mock_popen.return_value = mock_result
+
         authenticate_docker_to_ecr("us-west-2")
-        mock_subprocess.assert_called_with(
-            ["docker", "login", "-u", ANY, "-p", ANY, ANY], check=True
-        )
