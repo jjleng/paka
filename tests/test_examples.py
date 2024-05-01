a06902283a726e16c0b8889f36b3fa46026edc85
Jijun Leng <jijunleng@gmail.com>
2024-04-30 18:11:46 -0700

    tests: parse the cluster config yamls of examples

---
+++ tests/test_examples.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+
+from paka.config import parse_yaml
+
+examples_path = Path(__file__).parent.parent / "examples"
+
+
+@pytest.mark.parametrize(
+    "cluster_config",
+    [
+        examples_path / "website_rag" / "cluster.yaml",
+        examples_path / "invoice_extraction" / "cluster.yaml",
+        examples_path / "invoice_extraction" / "gpu_cluster.yaml",
+    ],
+)
+def test_example_configs(cluster_config: Path) -> None:
+    cluster_config = Path(cluster_config).expanduser().absolute()
+
+    if not cluster_config.exists():
+        raise FileNotFoundError(f"The cluster config file does not exist")
+
+    with open(cluster_config, "r") as file:
+        parse_yaml(file.read())
