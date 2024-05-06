4dd713be048372c68be2b743dfdc133492a508e5
Jijun Leng <jijunleng@gmail.com>
2024-05-06 00:10:19 -0700

    refactor: get rid of commands that interract with local cluster state

--- paka/cli/cluster.py
+++ paka/cli/cluster.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import os
-import shutil
-from pathlib import Path
 from typing import List
 
 import click
@@ -12,7 +9,6 @@ from paka.cli.utils import load_cluster_manager
 from paka.k8s.utils import remove_crd_finalizers
 from paka.k8s.utils import update_kubeconfig as merge_update_kubeconfig
 from paka.logger import logger
-from paka.utils import get_project_data_dir
 
 cluster_app = typer.Typer()
 
@@ -89,28 +85,7 @@ def down(
             except Exception as e:
                 pass
 
-        result = cluster_manager.destroy()
-
-        success_message = "The resources in the stack have been deleted"
-        if success_message in result.stdout:
-            # Remove the symlink to the current cluster and delete the cluster directory
-            current_cluster_path = Path(get_project_data_dir()) / "current_cluster"
-
-            # If the current cluster is the one being deleted, remove the symlink
-            if current_cluster_path.is_symlink():
-                current_cluster = current_cluster_path.resolve().name
-                if current_cluster == cluster_manager.config.cluster.name:
-                    current_cluster_path.unlink()
-
-            # Remove the cluster directory
-            cluster_dir = (
-                Path(get_project_data_dir())
-                / "clusters"
-                / cluster_manager.config.cluster.name
-            )
-
-            if cluster_dir.is_dir():
-                shutil.rmtree(cluster_dir)
+        cluster_manager.destroy()
 
 
 @cluster_app.command()
@@ -154,72 +129,3 @@ def refresh(
     """
     cluster_manager = load_cluster_manager(cluster_config)
     cluster_manager.refresh()
-
-
-@cluster_app.command()
-def list() -> None:
-    """
-    List all the clusters that being managed. Current cluster is marked with a "*".
-    """
-    current_cluster_path = Path(get_project_data_dir()) / "current_cluster"
-    current_cluster = (
-        os.readlink(current_cluster_path) if current_cluster_path.is_symlink() else None
-    )
-
-    found = False
-    for cluster in (Path(get_project_data_dir()) / "clusters").iterdir():
-        if cluster.is_dir():
-            prefix = (
-                "*"
-                if current_cluster
-                and cluster.resolve() == Path(current_cluster).resolve()
-                else " "
-            )
-            logger.info(f"{prefix} {cluster.name}")
-            found = True
-
-    if not found:
-        logger.info("No clusters found.")
-
-
-@cluster_app.command()
-def current() -> None:
-    """
-    Get the name of the current cluster.
-    """
-    current_cluster_path = Path(get_project_data_dir()) / "current_cluster"
-    current_cluster = (
-        os.readlink(current_cluster_path) if current_cluster_path.is_symlink() else None
-    )
-
-    if current_cluster:
-        logger.info(Path(current_cluster).name)
-    else:
-        logger.info("No current cluster found.")
-
-
-@cluster_app.command()
-def set_current(
-    cluster_name: str = typer.Argument(
-        ..., help="The name of the cluster to set as current."
-    )
-) -> None:
-    clusters_dir = Path(get_project_data_dir()) / "clusters"
-
-    cluster_dir = clusters_dir / cluster_name
-    current_link = Path(get_project_data_dir()) / "current_cluster"
-
-    if not cluster_dir.is_dir():
-        logger.info(f"Cluster {cluster_name} does not exist.")
-        raise typer.Exit(code=1)
-
-    if cluster_dir.resolve() != current_link.resolve():
-        if current_link.is_symlink():
-            current_link.unlink()
-
-        os.symlink(cluster_dir, current_link)
-
-    merge_update_kubeconfig()
-    logger.info(
-        f"Cluster {cluster_name} has been successfully set as the current cluster."
-    )
